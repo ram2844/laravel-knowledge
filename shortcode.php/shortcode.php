fbc82c61fd33137d9656be2c5d3985392591a854
Ram Yadav <rmy2134430@gmail.com>
2024-01-23 16:55:47 +0530

    dd

--- shortcode.php/shortcode.php
+++ shortcode.php/shortcode.php
@@ -3,4 +3,24 @@
 ->orderBy('age', 'desc') ==  ->latest('age')
 ->select('id', 'name')->get() ==  ->get(['id', 'name'])
 $request->has('value') ? $request->value : 'defaults' == $request->get('value', 'defaults')
+
+
+
+// <!-- Get random number id -->
+
+public function newId()
+{
+	$id = Str::random(24);
+}
+
+// get more than max limit data (100+ data) 
+
+$data == Models::get();
+
+$this->chunk(100, function($data)){
+	foreach ($data as $value) {
+		//Your code 
+	}
+}
+
 ?>
